c7d510994168bd384585959faa22e7935e179665
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-09-09 21:10:25 -0300

    Deploy heroku postgres

---
+++ src/main/java/com/concierge/apiblog/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.concierge.apiblog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(System.getenv("PG_URL"));
+        dataSource.setUsername(System.getenv("PG_USER"));
+        dataSource.setPassword(System.getenv("PG_PASSWORD"));
+
+        return dataSource;
+    }
+
+
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}

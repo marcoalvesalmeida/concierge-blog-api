37ae8eb3d42345a07cb4d6bafda2f77f6044a446
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-09-09 21:58:14 -0300

    Create initial Post Entity

---
+++ src/main/java/com/concierge/Models/Post.java
@@ -0,0 +1,33 @@
+package com.concierge.Models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="posts")
+public class Post implements Serializable {
+
+    private static  final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}

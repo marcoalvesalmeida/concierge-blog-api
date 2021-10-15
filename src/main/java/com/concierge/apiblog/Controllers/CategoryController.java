1a2cbcf67a122116d4d0a24bee8d863039a07390
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-10-15 12:02:38 -0300

    Create latest posts routes and crud categories and posts

---
+++ src/main/java/com/concierge/apiblog/Controllers/CategoryController.java
@@ -0,0 +1,51 @@
+package com.concierge.apiblog.Controllers;
+
+import com.concierge.apiblog.Models.Category;
+import com.concierge.apiblog.Repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.Serializable;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository _categoryRepository;
+
+    @RequestMapping(method= RequestMethod.GET, produces="application/json")
+    public List<Category> ListAll() {
+        return _categoryRepository.findAll();
+    }
+
+    @RequestMapping(method=RequestMethod.POST, produces="application/json", consumes="application/json")
+    public Serializable Create(@Valid @RequestBody Category category)
+    {
+        try {
+            return _categoryRepository.save(category);
+        }catch(Exception exception){
+            return exception;
+        }
+    }
+
+    @RequestMapping(path = {"/{id}"}, method=RequestMethod.PUT,  produces="application/json", consumes="application/json")
+    public ResponseEntity<Category> Update(@PathVariable Long id, @Valid @RequestBody Category category)
+    {
+        try {
+            if(!_categoryRepository.existsById(id)){
+                return ResponseEntity.notFound().build();
+            }
+
+            category.setId(id);
+            category = _categoryRepository.save(category);
+
+            return ResponseEntity.ok(category);
+        }catch(Exception exception){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
--- src/main/java/com/concierge/apiblog/Controllers/PostController.java
+++ src/main/java/com/concierge/apiblog/Controllers/PostController.java
@@ -3,28 +3,67 @@ package com.concierge.apiblog.Controllers;
 import com.concierge.apiblog.Models.Post;
 import com.concierge.apiblog.Repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/posts")
 public class PostController {
     @Autowired
     private PostRepository _postRepository;
 
-    @RequestMapping(value="/posts", method=RequestMethod.GET, produces="application/json")
+    @RequestMapping(method=RequestMethod.GET, produces="application/json")
     public List<Post> ListAll() {
         return _postRepository.findAll();
     }
 
-    @RequestMapping(value="/posts", method=RequestMethod.POST, produces="application/json", consumes="application/json")
-    public Post Create(@Valid @RequestBody Post post)
-    {
-        return _postRepository.save(post);
+    @RequestMapping(path = {"/latest/{qtd}/{type}", "/latest/{qtd}"}, method=RequestMethod.GET, produces="application/json")
+    public ResponseEntity<List<Post>> listNPosts(@PathVariable Integer qtd, @PathVariable Optional<String> type) {
+        try {
+            Pageable topTwenty = PageRequest.of(0, qtd);
+            List<Post> latestPosts;
+            if(type.isPresent() && type.get().equalsIgnoreCase("asc")){
+                latestPosts = _postRepository.findPostsByIdIsNotNullOrderByIdAsc(topTwenty);
+            }else {
+                latestPosts = _postRepository.findPostsByIdIsNotNullOrderByIdDesc(topTwenty);
+            }
+            return ResponseEntity.ok(latestPosts);
+        }catch (Exception exception){
+            return ResponseEntity.notFound().build();
+        }
     }
 
+    @RequestMapping(method=RequestMethod.POST, produces="application/json", consumes="application/json")
+    public Serializable Create(@Valid @RequestBody Post post)
+    {
+        try {
+            return _postRepository.save(post);
+        }catch(Exception exception){
+            return exception;
+        }
+    }
+
+    @RequestMapping(path = {"/{id}"}, method=RequestMethod.PUT,  produces="application/json", consumes="application/json")
+    public ResponseEntity<Post> Update(@PathVariable Long id, @Valid @RequestBody Post post)
+    {
+        try {
+            if(!_postRepository.existsById(id)){
+                return ResponseEntity.notFound().build();
+            }
+
+            post.setId(id);
+            post = _postRepository.save(post);
+
+            return ResponseEntity.ok(post);
+        }catch(Exception exception){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }

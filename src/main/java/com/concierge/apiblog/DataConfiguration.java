b9bf40f27d134a41a4d2259223efa850ff181c47
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-10-16 13:55:57 -0300

    Change data provider

--- src/main/java/com/concierge/apiblog/DataConfiguration.java
+++ src/main/java/com/concierge/apiblog/DataConfiguration.java
@@ -13,6 +13,7 @@ import java.net.URISyntaxException;
 
 import javax.sql.DataSource;
 
+ /*
 @Configuration
 public class DataConfiguration {
 	@Bean
@@ -29,6 +30,38 @@ public class DataConfiguration {
         basicDataSource.setPassword(password);
 
         return basicDataSource;
+    }
+}
+
+     */
+
+
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:15432/concierge");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("sarah");
+
+        return dataSource;
+    }
+
+
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
     }
 }
 

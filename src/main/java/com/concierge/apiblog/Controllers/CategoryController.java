8e035445a2f42eb3e7ee1094eb6f6b3e6e344403
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-10-27 21:02:54 -0300

    Fix cors

--- src/main/java/com/concierge/apiblog/Controllers/CategoryController.java
+++ src/main/java/com/concierge/apiblog/Controllers/CategoryController.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping(path = "/categories")
 public class CategoryController {
     @Autowired

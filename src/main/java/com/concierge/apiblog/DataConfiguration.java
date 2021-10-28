1b66bd1ce9ac036f580d5ffc1e47f82824777f19
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-10-27 21:07:34 -0300

    Fix database connect

--- src/main/java/com/concierge/apiblog/DataConfiguration.java
+++ src/main/java/com/concierge/apiblog/DataConfiguration.java
@@ -13,7 +13,7 @@ import java.net.URISyntaxException;
 
 import javax.sql.DataSource;
 
- /*
+
 @Configuration
 public class DataConfiguration {
 	@Bean
@@ -33,9 +33,9 @@ public class DataConfiguration {
     }
 }
 
-     */
 
 
+/*
 
 @Configuration
 public class DataConfiguration {
@@ -64,7 +64,7 @@ public class DataConfiguration {
         return adapter;
     }
 }
-
+*/
 
 
 
